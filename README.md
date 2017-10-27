8ae432a9421a7721e5d383a51d0df30d4d834b1f
Minh Chu <scsilver94@gmail.com>
2017-10-27 17:41:08 +0700

    initial commit :boom:

---
+++ README.md
@@ -0,0 +1,8 @@
+Local dev environment using Docker, NodeJs, Redis, MongoDB
+
+# Instruction  
+You must have Docker to run this repo
+
+# Usage  
+- Clone this repo
+- run `docker-compose up`
---
+++ api/index.js
@@ -0,0 +1,15 @@
+const restify = require('restify');
+
+function respond(req, res, next) {
+  res.send('hello ' + req.params.name);
+  next();
+}
+
+const server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+
+server.listen(3000, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
+
